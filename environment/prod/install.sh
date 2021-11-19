323a765e69f4e18580fbda305ca08341bda76d22
mamoru1112 <mamoru1112@gmail.com>
2021-11-19 13:32:01 +0900

    Create Grafana datasources and Configure Grafana dashboards

--- environment/prod/install.sh
+++ environment/prod/install.sh
@@ -1,4 +1,4 @@
-# Deploying DataGrid
+# DataGrid
 oc new-app httpd~./artifacts --name=httpd
 echo "sleep 60"
 sleep 60
@@ -7,27 +7,32 @@ echo "sleep 60"
 sleep 60
 
 oc apply -f create_data_grid.yaml
-#oc apply -f grafana_datasource.yaml
-#oc apply -f put_schema.yaml
 oc apply -f mycache.yaml
+oc apply -f put_schema.yaml
 
-# Deploying AMQ Streams cluster and topics
+# AMQ Streams
 oc apply -f create_kafka_cluster.yaml
 oc apply -f create_kafka_topic.yaml
 
-# Deploying Applications
+# Applications
 oc apply -f deploy_app.yaml 
 
 # Enabling monitoring for user-defined projects
 oc apply -f cluster-monitoring-config.yaml
 
-
 # Grafana
-#oc apply -f service-account.yaml
-#oc adm policy add-cluster-role-to-user cluster-monitoring-view -z infinispan-monitoring
-#export TOKEN=`oc serviceaccounts get-token infinispan-monitoring`
-#sed -e "s/__TOKEN__/${TOKEN}/g" grafana_datasource_template.yaml > grafana_datasource.yaml
-#oc apply -f create_grafana.yaml
+oc apply -f create_grafana.yaml
+oc apply -f service-account.yaml
+oc adm policy add-cluster-role-to-user cluster-monitoring-view -z infinispan-monitoring
+export TOKEN=`oc serviceaccounts get-token infinispan-monitoring`
+sed -e "s/__TOKEN__/${TOKEN}/g" grafana_datasource_template.yaml > grafana_datasource.yaml
+oc apply -f grafana_datasource.yaml
+oc apply -f infinispan-operator-config.yaml
+oc apply -f create_grafana_dashboard.yaml
+
+
+
+
 
 # clean up
 #oc delete is,bc,deploy,svc -l app=httpd
@@ -37,6 +42,7 @@ oc apply -f cluster-monitoring-config.yaml
 #oc delete cache tokencachedefinition
 #oc delete cache usercachedefinition
 #oc delete cache walletcachedefinition
+#oc delete batch mybatch
 #oc delete kafka my-cluster
 #oc delete kafkatopic point
 #oc delete kafkatopic payment
@@ -45,3 +51,7 @@ oc apply -f cluster-monitoring-config.yaml
 #oc delete svc payment
 #oc delete svc point
 #oc delete route payment
+#oc delete grafana example-grafana
+#oc delete sa infinispan-monitoring
+#oc delete grafanadatasource grafanadatasource
+#oc delete grafanadashboard simple-dashboard
