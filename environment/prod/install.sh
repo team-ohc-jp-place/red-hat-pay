6cfa4f7e1c4e4e9666feb7c3e33a6bc9265afdd8
Chihiro Ito <chiroito107@gmail.com>
2021-12-23 16:45:00 +0900

    表示するURLを修正

--- environment/prod/install.sh
+++ environment/prod/install.sh
@@ -40,5 +40,6 @@ oc apply -f servicemesh_memberroll-default.yaml
 oc apply -f servicemesh-gateway.yaml
 oc apply -f servicemesh_destination-rule-all.yaml
 
-oc -n istio-system get route istio-ingressgateway -o jsonpath='{.spec.host}'
+echo ""
+echo "Please access to http://`oc -n rhp-istio-system get route istio-ingressgateway -o jsonpath='{.spec.host}'`/index"
 
