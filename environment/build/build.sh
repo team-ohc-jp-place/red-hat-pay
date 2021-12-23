9c9e1416f06990104ed3f21b99aae0ec159936be
Chihiro Ito <chiroito107@gmail.com>
2021-12-23 15:45:40 +0900

    シェルのプロジェクト名を変更

--- environment/build/build.sh
+++ environment/build/build.sh
@@ -7,13 +7,14 @@ mvn clean package
 REGISTRY_URL=$(oc get route default-route -n openshift-image-registry --template={{.spec.host}})
 podman login -u $(oc whoami) -p $(oc whoami -t) ${REGISTRY_URL} --tls-verify=false
 
+export PROJECT_NAME=red-hat-pay
 # Building Application Images
 cd payment/
 podman build -f src/main/docker/Dockerfile.jvm -t quarkus/payment-jvm .
-podman image tag quarkus/payment-jvm:latest ${REGISTRY_URL}/${RED_HAT_PAY_PROJECT}/payment-jvm:latest
-podman push ${REGISTRY_URL}/${RED_HAT_PAY_PROJECT}/payment-jvm:latest --tls-verify=false
+podman image tag quarkus/payment-jvm:latest ${REGISTRY_URL}/${PROJECT_NAME}/payment-jvm:latest
+podman push ${REGISTRY_URL}/${PROJECT_NAME}/payment-jvm:latest --tls-verify=false
 cd ../point/
 podman build -f src/main/docker/Dockerfile.jvm -t quarkus/point-jvm .
-podman image tag quarkus/point-jvm:latest ${REGISTRY_URL}/${RED_HAT_PAY_PROJECT}/point-jvm:latest
-podman push ${REGISTRY_URL}/${RED_HAT_PAY_PROJECT}/point-jvm:latest
+podman image tag quarkus/point-jvm:latest ${REGISTRY_URL}/${PROJECT_NAME}/point-jvm:latest
+podman push ${REGISTRY_URL}/${PROJECT_NAME}/point-jvm:latest
 cd ..
