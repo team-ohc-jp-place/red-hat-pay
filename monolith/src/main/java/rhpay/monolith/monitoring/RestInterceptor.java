430710a845f1ffa6f27846ea52b3a86b66427746
Chihiro Ito <chiroito107@gmail.com>
2022-01-19 17:03:12 +0900

    REST時間にトランザクションが含まれない問題を解消

--- monolith/src/main/java/rhpay/monolith/monitoring/RestInterceptor.java
+++ monolith/src/main/java/rhpay/monolith/monitoring/RestInterceptor.java
@@ -6,12 +6,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Aspect
 @Component
+@Order(0)
 public class RestInterceptor {
 
     @Before("execution(* rhpay.monolith.rest.*.*(..))")
