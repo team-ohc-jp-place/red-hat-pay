ed5854bb5db32eacaba5b0638f0c75f85221ea0b
Chihiro Ito <chiroito107@gmail.com>
2022-02-01 13:44:14 +0900

    スキーマから古いクラスを削除

--- payment/src/main/java/rhpay/payment/system/PaymentSchema.java
+++ payment/src/main/java/rhpay/payment/system/PaymentSchema.java
@@ -4,8 +4,7 @@ import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 import rhpay.payment.cache.*;
 import rhpay.payment.task.PaymentFunction;
-import rhpay.payment.task.ProcessingFunction;
 
-@AutoProtoSchemaBuilder(includeClasses = {ShopperEntity.class, PaymentEntity.class, PaymentResponse.class, WalletEntity.class, ShopperKey.class, TokenKey.class, TokenEntity.class, TokenStatus.class, PaymentFunction.class, ProcessingEntity.class, ProcessingFunction.class}, schemaPackageName = "rhpay.payment.cache", autoImportClasses = true)
+@AutoProtoSchemaBuilder(includeClasses = {ShopperEntity.class, PaymentEntity.class, PaymentResponse.class, WalletEntity.class, ShopperKey.class, TokenKey.class, TokenEntity.class, TokenStatus.class, PaymentFunction.class}, schemaPackageName = "rhpay.payment.cache", autoImportClasses = true)
 public interface PaymentSchema extends GeneratedSchema {
 }
