461d37a76ba317cd311bc8c44a4c51c5778961f9
Chihiro Ito <chiroito107@gmail.com>
2022-02-04 16:51:52 +0900

    トークン作成のみのテスト

--- loadtest/locust/CreateToken.py
+++ loadtest/locust/CreateToken.py
@@ -9,11 +9,6 @@ class MultiUserTaskSet(TaskSet):
         response1 = self.client.post("/pay/" + user)
         if response1.status_code != 200:
             print (response1)
-        token = response1.json()["tokenId"]
-
-
-        response2 = self.client.request(method="POST", url="/pay/" + user + "/" + token + "/1/1")
-#        print (response2.json())
 
 class LoadTest(HttpLocust):
     task_set = MultiUserTaskSet
--- loadtest/locust/setDgTestCreateToken.sh
+++ loadtest/locust/setDgTestCreateToken.sh
@@ -2,5 +2,5 @@ set -e
 
 scriptDir=`dirname ${BASH_SOURCE[0]}`
 
-bash ${scriptDir}/seed.sh ${scriptDir}/MultiUserTaskSet.py http://`oc get route payment -n red-hat-pay --template={{.spec.host}}`
+bash ${scriptDir}/seed.sh ${scriptDir}/CreateToken.py http://`oc get route payment -n red-hat-pay --template={{.spec.host}}`
 
